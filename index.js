f518acebb1b020ad8f401ce864023d0e7dcd60cf
yahya moallim <yhy_565@hotmail.com>
2023-08-06 10:50:01 +0300

    gg

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 /* target the input fields */
 
-const merchantKey = document.querySelector('#merchant_key'); 
+const merchantKey = document.querySelector('#merchant_key');
+const operation = document.querySelector('#operation'); 
+const number = document.querySelector('#number'); 
+const amount = document.querySelector('#amount'); 
+const currency = document.querySelector('#currency'); 
+const description = document.querySelector('#description'); 
+const success_url = document.querySelector('#success_url'); 
+const hash = document.querySelector('#hash'); 
+
+
 
 
 /* a function to generate random data without the need to type it down */
